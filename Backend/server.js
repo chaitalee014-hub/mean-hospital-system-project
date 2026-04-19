dc89a536b62e6a50dce6ded177d677fed5f5c66e
Chaitalee Mahendra Mhamunkar <chaitalee014@gmail.com>
2026-04-19 22:53:52 +0530

    fix cors

--- Backend/server.js
+++ Backend/server.js
@@ -32,7 +32,7 @@ app.use(express.json());
 app.use(cors({
   origin: [
   "http://localhost:4200",
-  "mean-hospital-system.netlify.app",
+  "https://mean-hospital-system.netlify.app"
 ],
   credentials: true
 }));
