775200b58b691e91d9c01b5b6388f90c18f27553
Chaitalee Mahendra Mhamunkar <chaitalee014@gmail.com>
2026-04-19 22:59:09 +0530

    fix the angular routing

--- frontend/src/environment.ts
+++ frontend/src/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: false,
-  apiUrl: 'https://mean-hospital-system-project.onrender.com'
+  apiUrl: 'https://mean-hospital-system-project.onrender.com/api'
 };
 
