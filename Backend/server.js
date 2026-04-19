b76648549d0267df1c90a2e2cfe2ca63672ec955
Chaitalee Mahendra Mhamunkar <chaitalee014@gmail.com>
2026-04-19 21:20:49 +0530

    Updated API URL to production backend

--- Backend/server.js
+++ Backend/server.js
@@ -32,7 +32,7 @@ app.use(express.json());
 app.use(cors({
   origin: [
   "http://localhost:4200",
-  "https://mean-hospital-system-project.vercel.app"
+  "mean-hospital-system.netlify.app",
 ],
   credentials: true
 }));
